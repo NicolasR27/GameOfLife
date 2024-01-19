5d13e309a007e2de2ec09a33eec4c79c0dc58de9
Nic Rios <nickrios33@yahoo.com>
2024-01-19 09:26:55 -0800

    Initial Commit

---
+++ GameOfLife/GameOfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLifeApp.swift
+//  GameOfLife
+//
+//  Created by Nicolas Rios on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
